faf6b20743201c867a41f8a0794d64f026f71bbc
Iznobad <brandon.cebi.pro@gmail.com>
2024-08-29 10:52:33 +0200

    add create category request

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -7,11 +7,16 @@ import {
   Param,
   Delete,
   UseGuards,
+  Req,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { Request } from 'express';
+import { GetUser } from 'src/decorators';
+import { User } from '@prisma/client';
+import { UserPayload } from 'src/types';
 
 @UseGuards(AuthGuard)
 @Controller('categories')
@@ -19,8 +24,15 @@ export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  async create(
+    @Body() createCategoryDto: CreateCategoryDto,
+    @GetUser() user: UserPayload,
+  ) {
+    const create = await this.categoriesService.create(
+      createCategoryDto,
+      user.sub,
+    );
+    return create;
   }
 
   @Get()
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Request } from 'express';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createCategoryDto: CreateCategoryDto, userId: string) {
+    try {
+      const create = await this.prisma.category.create({
+        data: {
+          name: createCategoryDto.name,
+          userId,
+        },
+      });
+
+      return create;
+    } catch (err) {
+      throw new InternalServerErrorException('Internal server Error');
+    }
   }
 
   findAll() {
