34d565d19a243d846368af1039cfc79711f1a7a2
Iznobad <brandon.cebi.pro@gmail.com>
2024-08-30 09:10:58 +0200

    Create account crud

---
+++ src/accounts/accounts.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { AccountsService } from './accounts.service';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { UpdateAccountDto } from './dto/update-account.dto';
+import { GetUser } from 'src/decorators';
+import { UserPayload } from 'src/types';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+
+@UseGuards(AuthGuard)
+@Controller('accounts')
+export class AccountsController {
+  constructor(private readonly accountsService: AccountsService) {}
+
+  @Post()
+  create(@Body() createAccountDto: CreateAccountDto) {
+    return this.accountsService.create(createAccountDto);
+  }
+
+  @Get()
+  findAll(@GetUser() user: UserPayload) {
+    return this.accountsService.findAll(user.sub);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.accountsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateAccountDto: UpdateAccountDto) {
+    return this.accountsService.update(+id, updateAccountDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.accountsService.remove(+id);
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CategoriesModule } from './categories/categories.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { AccountsModule } from './accounts/accounts.module';
 import config from './config/config';
 
 @Module({
@@ -28,6 +29,7 @@ import config from './config/config';
     AuthModule,
     CategoriesModule,
     TransactionsModule,
+    AccountsModule,
   ],
 })
 export class AppModule {}
